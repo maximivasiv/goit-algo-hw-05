faf6d88c52e5166f7348a365b3653de56453c2e1
maximivasiv <maximivasiv1@gmail.com>
2026-04-15 20:59:39 +0300

    Create task_4.py

---
+++ task_4.py
@@ -0,0 +1,74 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Contact not found."
+        except IndexError:
+            return "Enter user name."
+    return inner
+
+
+@input_error
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+
+@input_error
+def change_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact updated."
+
+
+@input_error
+def show_phone(args, contacts):
+    name = args[0]
+    return contacts[name]
+
+
+@input_error
+def show_all(contacts):
+    if not contacts:
+        return "No contacts saved."
+    result = ""
+    for name, phone in contacts.items():
+        result += f"{name}: {phone}\n"
+    return result.strip()
+
+
+def main():
+    contacts = {}
+
+    while True:
+        command = input("Enter a command: ").strip().lower()
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command.startswith("add"):
+            args = command.split()[1:]
+            print(add_contact(args, contacts))
+
+        elif command.startswith("change"):
+            args = command.split()[1:]
+            print(change_contact(args, contacts))
+
+        elif command.startswith("phone"):
+            args = command.split()[1:]
+            print(show_phone(args, contacts))
+
+        elif command == "all":
+            print(show_all(contacts))
+
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()
